742a0623f32a21a77d2c583b5a8da595f414bc58
Stas Kozlov <kozostas@gmail.com>
2017-09-29 14:17:20 +0300

    29/09 side bar icons + style lines

--- src/app/side-menu-bar/side-menu-bar.component.ts
+++ src/app/side-menu-bar/side-menu-bar.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./side-menu-bar.component.css']
 })
 export class SideMenuBarComponent implements OnInit {
+  mennuList = ['Home', 'WorkFlow', 'Statistics', 'Calendar', 'Users', 'Settings'];
 
   constructor() { }
 
